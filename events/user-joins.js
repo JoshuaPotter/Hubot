850cd6002184806d184326a7ac7d740a7e6743fd
Joshua Potter <joshuaopotter@gmail.com>
2021-10-25 02:03:53 -0400

    Cleanup member add role event

--- events/user-joins.js
+++ events/user-joins.js
@@ -1,31 +1,27 @@
 /**
- * This file adds a role if they were a former member of the server and their
- * user_id is in the database. 
+ * This file adds a role to a user if their user_id is tracked in the database, indicating
+ * they previously were a member of the server.
  * https://discord.js.org/#/docs/main/stable/class/GuildMember
  */
-const { guildId } = require('../config.json');
 const { User } = require('../db-objects');
 
 module.exports = {
 	name: 'guildMemberAdd',
-	async execute(member, client) {
+	async execute(member) {
 		const { id } = member;
 
 		// Check if user was a former member by searching for their user ID in database
 		const user = await User.findOne({ where: { user_id: id } });
 
 		// User was a former member of the server
-		if (user !== null) {
-			// Add role
-			const guild = client.guilds.cache.get(guildId);
-			const role = guild.roles.cache.find(role => role.name.toLowerCase() === "rejoined");
-			const member = guild.members.cache.get(id);
-			member.roles.add(role);
+		if (user === null) return;
+		// Add role
+		const roleId = '';
+		member.roles.add(roleId);
 
-			// Delete user from db
-			await User.destroy({
-				where: { user_id: id },
-			});
-		}
+		// Delete user from db
+		await User.destroy({
+			where: { user_id: id },
+		});
 	},
 };
